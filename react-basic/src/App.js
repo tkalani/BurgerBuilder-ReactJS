f8e1b37b4edda0d895f4fb06f087e92c9a6e3245
tkalani <tanmay.k16@iiits.in>
2019-05-19 17:51:32 +0530

    List and Conditionals

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -6,31 +6,47 @@ class App extends Component {
 
   state = {
     persons: [
-      { name: "Tanmay", age: "21" },
-      { name: "Vaibhav", age: "19" },
-      { name: "Rajesh", age: "50" }
-    ]
+      { id:'weew', name: "Tanmay", age: "21" },
+      { id:'wefwef', name: "Vaibhav", age: "19" },
+      { id:'rgeg', name: "Rajesh", age: "50" }
+    ],
+    showPersons: false,
   }
 
-  switchNameHandler = (newName) => {
-    console.log('Clicked Here');
-    // this.state.persons[0].name = "Anita";
-    this.setState({
-      persons: [
-        { name: newName, age: "23" },
-        { name: "Vaibhav", age: "459" },
-        { name: "Rajesh", age: "54" }
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
+
+    // this.setState({
+    //   persons: [
+    //     { name: "Tanmay", age: "23" },
+    //     { name: event.target.value, age: "459" },
+    //     { name: "Rajesh", age: "54" }
+    //   ]
+    // })
   }
 
-  nameChangedHandler = (event) => {
+  deletePersonHandler = (index) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(index, 1);
+    this.setState({persons: persons});
+  }
+
+  showHidePerson = () => {
+    const doesShow = this.state.showPersons;
     this.setState({
-      persons: [
-        { name: "Tanmay", age: "23" },
-        { name: event.target.value, age: "459" },
-        { name: "Rajesh", age: "54" }
-      ]
+      showPersons: !doesShow
     })
   }
   
@@ -41,24 +57,44 @@ class App extends Component {
       padding: '8px',
       cursor: 'pointer'
     };
+
+    let persons = null;
+    if(this.state.showPersons)
+    {
+      persons = (
+        <div>
+            {this.state.persons.map((person, index) => {
+              return (
+                <Person 
+                  click={() => this.deletePersonHandler(index)}
+                  name={person.name} 
+                  age={person.age} 
+                  key={person.id}
+                  changed={(event) => this.nameChangedHandler(event, person.id)}  />
+              )
+            })}
+            {/* <Person 
+              name={this.state.persons[0].name} 
+              age={this.state.persons[0].age} />
+            <Person 
+              name={this.state.persons[1].name} 
+              age={this.state.persons[1].age}
+              click={this.switchNameHandler.bind(this, 'Tanmay!')}
+              changed={this.nameChangedHandler} />
+            <Person 
+              name={this.state.persons[2].name} 
+              age={this.state.persons[2].age} /> */}
+          </div>
+      )
+    }
     return (
       <div className="App">
         <h1>Hi, I am Tanmay.</h1>
         <p>This is working</p>
         <button 
           style={style}
-          onClick={() => this.switchNameHandler('Tanmay...')}>Switch Name</button>
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age} />
-        <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Tanmay!')}
-          changed={this.nameChangedHandler} />
-        <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age} />
+          onClick={this.showHidePerson}>Show / Hide</button>
+        {persons}
         {/* <Person name="Tanmay" age="21">My hobby is painting.</Person>
         <Person name="Vaibhav" age="19"/> */}
       </div>
